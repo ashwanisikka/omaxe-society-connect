40e34c1e4897d3f407e2c1bf0be77e1445685c2c
LAYMAN <ashwani.sikka@gmail.com>
2026-05-18 05:42:01 +0530

    Remove AuthProvider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import App from './App.tsx';
-import { AuthProvider } from './contexts/AuthContext'; 
 import './index.css';
 
 const firebaseConfig = {
@@ -19,8 +18,6 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </StrictMode>
 );
